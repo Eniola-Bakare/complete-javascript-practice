252a13396c426f7507c40b3b8926759dd47bd5e3
Eniola Bakare <eebakare@gmail.com>
2023-04-07 17:59:51 +0100

    working on advanced dom

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -2,13 +2,14 @@
 
 ///////////////////////////////////////
 // Modal window
-
+const header = document.querySelector('.header')
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault()
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,14 +19,27 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal))
 
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
+  btnCloseModal.addEventListener('click', closeModal);
+  overlay.addEventListener('click', closeModal);
 
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
-  }
+  document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+      closeModal();
+    }
 });
+
+
+const message = document.createElement('div')
+message.classList.add('cookie-message')
+message.innerHTML = 'We use cookies to improve functionality and analytics. <button class="btn btn--close-cookie">Got it! </button>'
+
+// header.prepend(message)
+// header.append(message)
+
+header.before(message)
+// header.after(message)
+document.querySelector('.btn--close-cookie').addEventListener('click', function(){
+  message.remove()
+})

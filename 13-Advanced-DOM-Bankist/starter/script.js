e9ef080df8b25cb3e2ff5de8268d2305f4236613
Eniola Bakare <eebakare@gmail.com>
2023-05-02 19:58:58 +0100

    learning javascript - highlight: learning intersection observer

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -7,6 +7,10 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const nav = document.querySelector('.nav')
+const logo = document.querySelector('#logo')
+const allSections = document.querySelectorAll('.section')
+const imgTarget = document.querySelectorAll('img[data-src]')
 
 const openModal = function (e) {
   e.preventDefault()
@@ -43,3 +47,137 @@ header.before(message)
 document.querySelector('.btn--close-cookie').addEventListener('click', function(){
   message.remove()
 })
+
+
+const learMoreBtn = document.querySelector('.btn--scroll-to')
+const section1 = document.querySelector('#section--1')
+
+learMoreBtn.addEventListener('click', function(e){
+  const s1cords = section1.getBoundingClientRect()
+  section1.scrollIntoView({behavior: 'smooth'})
+})
+
+// handling multiple events traditionally
+// document.querySelectorAll('.nav__link').forEach(function(eachItem) {
+//   eachItem.addEventListener('click', function (e){
+//     e.preventDefault()
+//     const id = this.getAttribute('href')
+//     console.log(id)
+//     document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+//   })
+//   })
+
+
+// using event delegation
+// 1. attach the event listener to the parent element of such concerned els
+// 2. Define a matching strategy
+// 3. Determine what element originated the event and mke it receive the function
+  document.querySelector('.nav__links').addEventListener('click', (e)=>{
+    e.preventDefault()
+    // console.log(e.target)
+
+    if(e.target.classList.contains('nav__link')){
+      const id = e.target.getAttribute('href')
+      // console.log(id)
+      document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+    }
+  })
+
+  // traversing the dom - walking through the dom. selecting element relative to other elements
+  // for child elements/nodes; parentElement&& grandParent elements too; for sibling elements
+
+  // implementing faded automation
+
+  // refactoring to ensure DRY
+  const handleHover = function(e){
+    if(e.target.classList.contains('nav__link')){
+      const currLink = e.target
+
+      const siblings = currLink.closest('nav').querySelectorAll('.nav__link')
+      siblings.forEach(sib => {
+        if (sib !== currLink) {
+          sib.style.opacity = this;
+        }
+      })
+      logo.style.opacity = this;
+
+    }
+  }
+
+  nav.addEventListener('mouseover', handleHover.bind(0.5))
+
+  nav.addEventListener('mouseout', handleHover.bind(1))
+
+  const initialCords = section1.getBoundingClientRect().top
+  // console.log(initialCords)
+
+  // window.addEventListener('scroll', function(e){
+  //   console.log(initialCords, 'initialCords')
+  //   console.log(this.window.scrollY, 'scroll Y')
+
+  //   if (this.window.scrollY > initialCords) nav.classList.add('sticky')
+  //   else nav.classList.remove('sticky')
+  // })
+
+  const navHeight = nav.getBoundingClientRect().height
+
+  const observerCallBack = function(entries, headerObserver){
+    const [entry] = entries
+    // console.log(entry)
+    if (!entry.isIntersecting) nav.classList.add('sticky')
+    else nav.classList.remove('sticky')
+  }
+
+  const headerObserver = new IntersectionObserver (observerCallBack, {
+    root:null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`
+  })
+
+  headerObserver.observe(header)
+
+
+
+  // reveal sections observer
+
+  const revealSection = function(entries, observer){
+    const [entry] = entries
+    // console.log(entry.target.id)
+    if (!entry.isIntersecting) return;
+    else entry.target.classList.remove('section--hidden')
+    observer.unobserve(entry.target)
+  }
+
+  const sectionObserver = new IntersectionObserver (revealSection, {
+    root: null,
+    threshold: 0.15,
+  })
+
+  allSections.forEach(section =>{
+    // console.log(section)
+    sectionObserver.observe(section)
+    section.classList.add('section--hidden')
+  })
+
+  // intersection observer for the images
+  const loadImg = function(entries, oberver){
+    const [entry] = entries
+    const dataSetSrc = entry.target.getAttribute('data-src')
+
+    if(!entry.isIntersecting) return;
+    entry.target.src = dataSetSrc
+    entry.target.addEventListener('load', () => {
+      entry.target.classList.remove('lazy-img')
+    })
+
+    oberver.unobserve(entry.target)
+  }
+  const imgObserver = new IntersectionObserver(loadImg, {
+    root: null,
+    threshold: 0,
+    rootMargin: '200px'
+  })
+
+  imgTarget.forEach(img => {
+    imgObserver.observe(img)
+  })

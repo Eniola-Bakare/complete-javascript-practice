6acb1a93b23dc0b696512b41d1f4b3dd7ece7a50
Eniola Bakare <eebakare@gmail.com>
2023-07-03 14:54:01 +0100

    completed coding challenge 2

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -2,6 +2,7 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
+const imgContainer = document.querySelector('.images')
 
 ///////////////////////////////////////
 
@@ -23,9 +24,9 @@ const countriesContainer = document.querySelector('.countries');
 // .then(data => console.log(data))
 
 // XMLHTTPREQUEST
-const REQUESTXML = new XMLHttpRequest()
-  REQUESTXML.open('GET', `https://jsonplaceholder.typicode.com/todos/2`)
-  REQUESTXML.send()
+// const REQUESTXML = new XMLHttpRequest()
+//   REQUESTXML.open('GET', `https://jsonplaceholder.typicode.com/todos/2`)
+//   REQUESTXML.send()
 
   // REQUESTXML.addEventListener('load', function(){
   //   console.log(REQUESTXML.responseText)
@@ -106,3 +107,57 @@ TEST COORDINATES 2: -33.933, 18.474
 // getLocationn().then(res => console.log(res)).catch(err => console.error(err.message))
 
 // console.log('Getting Locale')
+
+
+///////////////////////////////////////
+// Coding Challenge #2
+
+/* 
+Build the image loading functionality that I just showed you on the screen.
+
+Tasks are not super-descriptive this time, so that you can figure out some stuff on your own. Pretend you're working on your own 😉
+
+PART 1
+1. Create a function 1️⃣ 'createImage' which receives imgPath as an input. This function 2️⃣returns a promise which creates a new image (use document.createElement('img')) and sets the .src attribute to the provided image path. When the image is done loading, append it to the DOM element with the 'images' class, and resolve the promise. The fulfilled value should be the image element itself. In case there is an error loading the image ('error' event), reject the promise.
+
+If this part is too tricky for you, just watch the first part of the solution.
+
+PART 2
+2. Comsume the promise using .then and also add an error handler;
+3. After the image has loaded, pause execution for 2 seconds using the wait function we created earlier;
+4. After the 2 seconds have passed, hide the current image (set display to 'none'), and load a second image (HINT: Use the image element returned by the createImage promise to hide the current image. You will need a global variable for that 😉);
+5. After the second image has loaded, pause execution for 2 seconds again;
+6. After the 2 seconds have passed, hide the current image.
+
+TEST DATA: Images in the img folder. Test the error handler by passing a wrong image path. Set the network speed to 'Fast 3G' in the dev tools Network tab, otherwise images load too fast.
+*/
+
+// Solution attempt
+const createImage = function(imgPath){
+  return new Promise(function (resolve, reject){
+    const imgDiv = document.createElement('img')
+    imgDiv.src = imgPath
+    imgDiv.addEventListener('load', resolve),
+    reject
+  })
+}
+
+createImage('./img/img-1.jpg')
+  .then(res =>
+    imgContainer.insertAdjacentElement('afterbegin', res.target
+  ))
+  .then(res => {
+    setTimeout(function(){
+      imgContainer.style.display = 'none'
+      res.src = './img/img-2.jpg'      
+    }, 2000)
+  })
+  .then(res => {
+    setTimeout(function () {
+      console.log('my timeee')
+      imgContainer.style.display = 'flex'
+    }, 4000)
+  })
+  .catch(err => console.log(`${err} 'I can\'t find you image'`))
+
+ 
